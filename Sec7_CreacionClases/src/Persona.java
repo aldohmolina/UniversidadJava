af35a978d741243e4e8fa0c454685c3f050f416b
aldohmolina <aldohmolina@gmail.com>
2019-06-19 13:22:48 -0500

    Sec7 Clase Persona

---
+++ Sec7_CreacionClases/src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+
+    public void desplegarNombre() {
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellidoPaterno = " + apellidoPaterno);
+        System.out.println("apellidoMaterno = " + apellidoMaterno);
+    }
+}
